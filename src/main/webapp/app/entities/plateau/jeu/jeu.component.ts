d01a1dae99bc7d87ade86892144a758cd8664031
YA <a.yannick.lefeuvre@gmail.com>
2023-02-21 22:14:50 +0100

    avec matmodule

--- src/main/webapp/app/entities/plateau/jeu/jeu.component.ts
+++ src/main/webapp/app/entities/plateau/jeu/jeu.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Inject } from '@angular/core';
 import { EventManager } from '@angular/platform-browser';
 import { DataUtils } from 'app/core/util/data-util.service';
 import { PlateauService } from '../service/plateau.service';
@@ -10,6 +10,13 @@ import { Subject } from 'rxjs';
 import { JoueurService } from 'app/entities/joueur/service/joueur.service';
 import { HttpResponse } from '@angular/common/http';
 import { Caze } from 'app/entities/caze/caze.model';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+export interface DialogData {
+  question: string;
+  reponse: string;
+}
 
 @Component({
   selector: 'jhi-jeu',
@@ -21,6 +28,8 @@ export class JeuComponent implements OnInit {
   joueur: Joueur | null = null;
   isLoading = false;
   joueurs?: IJoueur[];
+  question?: string | null = null;
+  reponse?: string | null = null;
 
   private readonly destroy$ = new Subject<void>();
 
@@ -31,7 +40,8 @@ export class JeuComponent implements OnInit {
     protected elementRef: ElementRef,
     protected activatedRoute: ActivatedRoute,
     protected plateauservice: PlateauService,
-    protected joueurService: JoueurService
+    protected joueurService: JoueurService,
+    public dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -74,5 +84,32 @@ export class JeuComponent implements OnInit {
       .findPrincipal()
       .pipe(takeUntil(this.destroy$))
       .subscribe(plateau => (this.plateau = plateau.body));
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: { name: this.question, animal: this.reponse },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.reponse = result;
+    });
+  }
+
+  // jouer(joueur:Joueur):void{
+  //   joueur.
+  //  }
+}
+
+@Component({
+  selector: 'jhi-jeu',
+  templateUrl: './jeu.component.html',
+})
+export class DialogOverviewExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogOverviewExampleDialog>, @Inject(MAT_DIALOG_DATA) public question: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
